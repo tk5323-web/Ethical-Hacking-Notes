311e5021a775cefe6c846703c67dbb0a5e4de18b
tk5323-web <tk5323@srmist.edu.in>
2026-03-22 10:41:06 +0530

    Create Port_Scanner.py

---
+++ Port_Scanner.py
@@ -0,0 +1,14 @@
+import socket
+
+target = "127.0.0.1"  # localhost
+ports = [21, 22, 80, 443]
+
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((target, port))
+    if result == 0:
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is CLOSED")
+    sock.close()
